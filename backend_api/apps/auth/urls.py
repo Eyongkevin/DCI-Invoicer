a4b3bbd17ea2289f26af3d00f819648cdd75221a
Eyongkevin <tonyparkerkenz@gmail.com>
2023-11-24 12:14:17 +0100

    add refresh token

--- backend_api/apps/auth/urls.py
+++ backend_api/apps/auth/urls.py
@@ -6,7 +6,9 @@ router = routers.SimpleRouter()
 
 router.register(r"v1/auth/login", views.LoginViewSet, basename="v1-auth-login")
 router.register(r"v1/auth/register", views.RegisterViewSet, basename="v1-auth-register")
-
+router.register(
+    r"v1/auth/refresh-token", views.RefreshViewSet, basename="v1-auth-refresh-token"
+)
 urlpatterns = [
     *router.urls,
 ]
--- backend_api/apps/auth/views.py
+++ backend_api/apps/auth/views.py
@@ -6,7 +6,11 @@ from rest_framework.permissions import AllowAny, BasePermission
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    TokenRefreshSerializer,
+)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.core.exceptions import (
     UserLoginFailedEx,
@@ -80,3 +84,16 @@ class RegisterViewSet(ViewSet):
         res: Dict[str, str] = {"username": user.username, "password": password}
 
         return Response(res, status=status.HTTP_201_CREATED)
+
+
+class RefreshViewSet(ViewSet, TokenRefreshView):
+    permission_classes = (AllowAny,)
+    http_method_names = ("post",)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as err:
+            raise InvalidToken(err.args[0]) from err
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
