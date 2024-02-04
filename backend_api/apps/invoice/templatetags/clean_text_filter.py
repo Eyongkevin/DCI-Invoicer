47ac7742314ca84236cdb53182b92dc860221727
Eyongkevin <tonyparkerkenz@gmail.com>
2024-02-04 20:05:02 +0100

    add nested list to handle nested tasks

--- backend_api/apps/invoice/templatetags/clean_text_filter.py
+++ backend_api/apps/invoice/templatetags/clean_text_filter.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def remove_dash(value: str) -> str:
     return value.strip("-").strip()
+
+
+@register.filter
+def is_nested(value: str) -> bool:
+    return value.startswith("-")
